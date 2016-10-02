56e7d785e48f2d5fe42b7ab662ad3f9de39c593c
Colleen <Colleenpamchen@gmail.com>
2016-10-02 11:23:02 -0700

    finished p1

--- Homework1.m
+++ Homework1.m
@@ -1,59 +1,63 @@
 %% Simulate stimuli (up direction 1 / down direction 0) 
 % for the 100 trials at each of the 3 coherence levels
+clc
+clear all
+close all
 
 ntrials = 100; 
-
-coherence = [0.8; 3.2; 12.8]; % coherence level of the stimuli presented in the 100 trials 
-lambda = [20; 20; 40]; % mean distribution of the firing rate responding to the UP preferred direction at each of the respective coherence level
-
-z = [1:100]'; % z threshold applied to discriminate the poissan firing rate distributions between UP and DOWN preferred directions   
-% i.e. If r >= z, report "UP" otherwise "DOWN" direction preference 
+coherence = [0.8,3.2,12.8]% coherence level of the stimuli presented in the 100 trials 
+lambdaup = [20;20;40]; % mean distribution of the firing rate responding to the UP preferred direction at each of the respective coherence level
+lambdadown = [18;18;18];
 
 for (n=1:length(coherence))
     
-stimuli = round(rand(ntrials,1)); % stimulus presented at each of the 100 trials 
-r = poissrnd(lambda(n),[ntrials,1]); % poissand firing rate distribution for preferred PLUS direction 
+% stimuli = round(rand(ntrials,1)); % stimulus presented at each of the 100 trials
+% 1 encode up direction, 0 encode down
+upRate(n,:) = poissrnd(lambdaup(n),[ntrials,1])'; % poissand firing rate distribution for preferred PLUS direction 
+downRate(n,:) = poissrnd(lambdadown(n), [ntrials,1])'; 
 
-for (i=1:length(stimuli))
-predicted(i) = ( r(i) >= z(i) ); % Predicted behavioral response based on applying r, firing rate data, to a z threshold. 
-% UP direction is encoded 1; DOWN is 0 
-hit(i) = logical(predicted(i) == stimuli(i)); % hit 
-% fa = logical( find(stimuli == 0 & predicted ==1)); % false alarm
-fa(i) = logical( stimuli(i) == 0 & predicted ==1 );
+z = [0:max(upRate)]; % z threshold applied to discriminate the poissan firing rate distributions between UP and DOWN preferred directions   
+% i.e. If r >= z, report "UP" otherwise "DOWN" direction preference 
 
-% end
+pHit = zeros(length(z),length(coherence));
+pFA =  zeros(length(z),length(coherence));
 
+    for zi = 1:length(z)
+        threshold = z(zi);
+        pHit(zi,n) = sum(upRate(n,:)>threshold)/ntrials;
+        pFA(zi,n) = sum(downRate(n,:)>threshold)/ntrials;
+    end
+    
+figure;
+clf
+plot(pFA,pHit,'.-');
 
-beta(i) = sum(hit)/ntrials; 
-alpha(i) = sum(fa)/ntrials; 
+legend(num2str(coherence(n)),'Location','SouthEast');
+set(gca,'XLim',[0,1]);
+set(gca,'YLim',[0,1]);
 
-
-figure(n); 
 title('ROC Curve for z threshold at each motion coherence level','FontSize',12,'FontWeight','bold')
-axis([0,1,0,1])
 xlabel('\alpha (false alarm rate)','FontSize',12,'FontWeight','bold') % x-axis label
-ylabel('\beta (hit rate)','FontSize',12,'FontWeight','bold') % y-axis label
-legend('% coherence level')
-
-hold on ;
-plot(alpha(i), beta(i),'x');
-
-
+ylabel('\beta (hit rate)','FontSize',12,'FontWeight','bold') % y-axis label  
+set(gca,'XTick',0:.2:1);
+set(gca,'YTick',0:.2:1);
+hold on
+plot([0,1],[0,1],'k-');
+axis square
+   
 end
 
 
 
-end
-
-
-%% for each of the 100 stimuli, at each coherence level, apply the z threshold to the 
-% averaged firing rate r (which comes from the neural data, ranged 0-60) to predict behavioral response 
-% for directional prefernce (+/-). 
 
 
 
 
-%% calculate the alpha (hit rate) and beta (false alarm) rates as datapoints over 100 trials
+
+
+
+%%
+
 
 
 
