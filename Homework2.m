ef06e25539684b4dce4fd0fa8c9c146918020bfa
Colleen <Colleenpamchen@gmail.com>
2016-10-17 09:25:13 -0700

    friday progress

--- Homework2.m
+++ Homework2.m
@@ -16,7 +16,7 @@ Rm = 10;
 V = NaN(length(t)+1,1); % membrane potential mV 
 spikes = NaN(length(t),1);
 
-Ie =[ 1.6:0.1:2.5];  % array of injected current 
+Ie =[ 1;2;3;6;10];  % array of injected current 
 V(1) = -65; % initialized at -65 mV
 
 for ie = 1:length(Ie) 
@@ -34,23 +34,19 @@ spikecount(ie) = sum(nonzeros(spikes)) % frequency: numb of times it spiked
 
 title('Spike Trains','FontSize',12,'FontWeight','bold')
 xlabel('t (ms)','FontSize',12,'FontWeight','bold') 
-ylabel('voltage (mV)','FontSize',12,'FontWeight','bold') 
+ylabel('membrane voltage (mV)','FontSize',12,'FontWeight','bold') 
 
-figure(1); %cla;
-hold on
-subplot(10,1,ie); 
+figure;
 plot(t(1:100),V(1:100));
 legend( strcat('current ', num2str(Ie(ie)), ' mV' ) ,'Location','NorthEast');
 
 end
-
 
 figure 
 plot(Ie, spikecount)
 title('FL plot','FontSize',12,'FontWeight','bold')
 xlabel('Current','FontSize',12,'FontWeight','bold') 
 ylabel('Frequency','FontSize',12,'FontWeight','bold') 
-% legend( num2str(sig(i)) ,'Location','SouthEast');
 
 
 %% Izhikevich Neurons
