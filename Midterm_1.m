5cdfad0e84f7991f929c20801e9c4a9b944de343
Colleen <Colleenpamchen@gmail.com>
2016-11-08 12:19:02 -0800

    more thoughts

--- Midterm_1.m
+++ Midterm_1.m
@@ -1,16 +1,24 @@
 %% Midterm Assignment
 
 % Make gradient 
-freq = 8;
-angle = 90; 
-Period= 128;
-xx = freq * cosd(angle);
-yy = freq * sind(angle);
-[X Y] = meshgrid(1:Period);
-grating = cos(2*pi*((xx*X/Period) +(yy*Y/Period)));
-imshow(grating); 
+% freq = 8;
+% angle = 90; 
+% Period= 128;
+% xx = freq * cosd(angle);
+% yy = freq * sind(angle);
+% [X Y] = meshgrid(1:Period);
+% grating = cos(2*pi*((xx*X/Period) +(yy*Y/Period)));
+% imshow(grating); 
+% 
+% C = 50; % contrast 
+% 0.5* C*X
 
-C = 50; % contrast 
-0.5* C*X
+% Make Stimulus:
+angle = [0:45:135];
+contrast = [0;12;25;50]; 
+degree = [cos(angel);sin(angle)];
+
+
+
 
 
