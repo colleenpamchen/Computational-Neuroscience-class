00597b5ad3b85b8792feb4b84bfa2fcd282722e4
Colleen Pam Chen <colleenpamchen@gmail.com>
2016-11-08 14:41:23 -0800

    changed to functions

--- Midterm_2.m
+++ Midterm_2.m
@@ -1,3 +1,11 @@
-%% Midterm_2.m 
+%% Midterm_2.m #2 Dopamine modulated STDP solve "distal reward problem"
+% (1) Implementation: 
+%
+% (2) Experimental Design: 
+%
+% (3) Expected Results: 
+%
+%
+
 
 
