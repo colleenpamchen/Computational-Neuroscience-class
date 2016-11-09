0c86d8207ce63f836b0175df17e794fcfb5029e1
Colleen Pam Chen <colleenpamchen@gmail.com>
2016-11-09 14:01:31 -0800

    almost there

--- Midterm_1.m
+++ Midterm_1.m
@@ -10,25 +10,98 @@
 function cc_midterm1
 
 % Make Stimulus:
-prefDir = [-45:45:135]; % preferred orientation (degrees) 
-contrast = [0;12;25;50;75]; % ??????????? HOW to use contrast with orientation?? 
-degree = [ cos(prefDir);sin(prefDir) ];
+pd = [0:180]; % preferred orientation (degrees) 
+contrast = [0;12;25;50;75]; % ??????????? HOW to use contrast with pd ?? 
+% degree = [ cos(pd);sin(pd) ];
 
 
+% 7 Input Neurons: InpN 
 % how do I create input neurons using cosine turning curves? 
-s = -90:1:180; % orientation angles in degrees
-smax= prefDir; % preferred orientation 
-fr=zeros(length(prefDir),length(s)); 
-rmax= 52.14;
-sig= 14.73;
+angle = [0;22.5;45;90;112.5;135;180]; % input NEURON orientation angles in degrees
+numInp = length(angle);
+numOut = length(angle);
 
-r = cosTune(,);
+% allocate and initialize neurons
+inpN = zeros(1,numInp); % inpN =zeros(length(pd),length(angle)); 
+outN = zeros(numInp,numOut);
 
+% allocate and initialize 3 sets of weights: 
+% 1. Excitatory weights from Input to Output neurons: 
+wInpOutExc = zeros(numInp, numOut)+1; % initialized to 1
+    % the input to output weights are set to make horizaontal directions have
+    % stronger weights projecting to the horizaontal output neuron and vice versa
+    Vertical = 4; 
+    wInpOutExc(Vertical,:) = 1.42; 
 
+% 2. InputNeurons to INHIBITORY neuron:
+wInpINH = zeros(numInp,1);
+
+% 3. INHIBITORY neuron to OutputNeurons: 
+wINHout = zeros(1,numOut)-1; % initialized to -1 
+
+for a=1:numInp
+    
+    % set the rates of the input neurons
+    for p=1:length(pd) 
+        inpN(p) = cosTune(pd(p), angle(a));
+    end
+    
+    for i=1:numOut
+    % initialize SynIn
+    SynIn = 0 ; 
+    
+        for j = 1:numInp
+            
+            % (1) SynIn = SynIn + InpN .* wInpOutExc
+            SynIN =  SynIn + inpN(j) .* wInpOutExc(j,i) ;
+            % (2) SynIn = SynIn + InpN .* wInpINH
+            SynIn = SynIn + inpN(j) .* wInpINH(j) ;
+            
+            if j==1
+            outN(1) = rand(1);    
+            % (3) SynIn = SynIn + InpN .* wINHout
+            SynIn = SynIn + outN(j) .* wINHout(i) ;
+            else 
+            SynIn = SynIn + outN(j-1) .* wINHout(i) ;    
+            end
+          
+            disp(SynIn) 
+            
+        end
+   
+        % Output Neurons: OutN
+        % set initial state of Output neurons to random numbers. 
+        % outN(1,:) = rand(1,numOut); % start at random initial orientation
+
+    outN(a,i) = sigmoidN (SynIn);    
+    
+    fr(i) =  sum(outN(:,i);  
+    
+    end
+   
+    plot(pd,fr);
+    
+    
 end
 
-function r = cosTune(pd, a)
-r = abs(cos(a-pd));
+
+end % end of user-defined function
+
+function r = cosTune(pd, s)
+% r = abs(cos(a-pd));
+% Gaussian Tuning Curves: 
+rmax= 52.14; % parameter from Dayan&Abbot 
+sig= 14.73; % parameter from Dayan&Abbot 
+r = rmax * exp( (-1/2)* ( (s-pd)/sig ).^2 );
+end
+
+% Sigmoid Activation
+%   Input:  I - synaptic input
+%           g - gain
+%   Output: firing rate of sigmoid function
+function r = sigmoidN (I) 
+g=2; 
+r = 1/(1+exp(-g*I));
 end
 
 
@@ -58,11 +131,7 @@ end
 % 
 % end
 
-
 % dot(INH,w_inhibitory);
-
-
-
 
 % Make gradient 
 % freq = 8;
