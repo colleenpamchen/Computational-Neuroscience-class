b452f9aa3ddbb6bb4f60022776781a24ca56278c
Colleen <Colleenpamchen@gmail.com>
2016-11-14 01:25:12 -0800

    not much progress

--- Midterm_1.m
+++ Midterm_1.m
@@ -18,15 +18,14 @@ contrast = [0;12;25;50;75]; % ??????????? HOW to use contrast with pd ??
 Time=100; 
 
 % 7 Input Neurons: InpN 
-% how do I create input neurons using cosine turning curves? 
 Npd = [0,22.5,45,90,112.5,135,180]; % input NEURON orientation angles in degrees
 
 numInp = length(Npd);
 numOut = length(Npd);
 
 % allocate and initialize neurons
-inpN = zeros(numInp,length(s)); % inpN =zeros(length(pd),length(angle)); 
-outN = zeros(Time,length(s));
+inpN = zeros(numInp,length(s)); % 
+outN = zeros(numOut,Time);
 
 % allocate and initialize 3 sets of weights: 
 % 1. Excitatory weights from Input to Output neurons: 
@@ -37,7 +36,7 @@ wInpOutExc = repmat(wext(:),1,length(s));
     % the input to output weights are set to make horizaontal directions have
     % stronger weights projecting to the horizaontal output neuron and vice versa
     Vertical = 4; 
-    wInpOutExc(Vertical,:) = 10; 
+    wInpOutExc(Vertical,:) = 5; 
 
 % 2. InputNeurons to INHIBITORY neuron:
 winp = [.5,.5,.5,.5,.5,.5,.5];
@@ -45,24 +44,21 @@ wInpINH = repmat(winp(:),1,length(s));
 
 % 3. INHIBITORY neuron to OutputNeurons: 
 winh=[-1,-1,-1,-1,-1,-1,-1]; % initialized to -1 
-wINHout = repmat(winh(:),1,length(s)) ; 
+wINHout = repmat(winh(:),1,Time) ; 
 
-outN(1,:) = rand(1,length(s));    
+outN(:,1) = rand(length(Time),1);    
 
-for a=1:numInp % neuron's ANGLE loop 
-       for p=1:length(s) % pd loop for inpN activity 
-            inpN(a,p) = cosTune(s(p), Npd(a));
-            
-       end
+for a=1:numInp % neuron's preferred ANGLE loop 
        
-% figure;
-% plot(Npd,inpN(a,:))
-% hold on
-% contrast(a,:) = inpN(a,:) .* (0.5 / (sigma + 0.5) );
-% plot(Npd,contrast(a,:)); 
-% hold off 
+    for si=1:length(s) % loop through 361 degrees for inpN activity 
+        inpN(a,si) = cosTune(s(si), Npd(a));     
+    end
+    
+end
 
-       for t=2:Time
+% inpN 7 x 361 
+
+for t=2:Time
     % set the rates of the input neurons
      
         % cycle through the output neurons
@@ -72,29 +68,39 @@ for a=1:numInp % neuron's ANGLE loop
             for j = 1:numInp % loop through InputN indexed 'j' 
                 % (1) 1-1 Input -> Output EXC : SynIn = SynIn + InpN .* wInpOutExc
 %                 SynIn =  SynIn + inpN(j) .* wInpOutExc(j,i) ;
-                SynIn = inpN(j,:) .* wInpOutExc(j,:) ;
+                SynIn = dot (inpN(j,:) , wInpOutExc(j,:) ) ;
                 % (2) all-1 Input -> INH : SynIn = SynIn + InpN .* wInpINH
 %                 SynIn = SynIn + inpN(j) .* wInpINH(j) ;  
-                SynIn = SynIn + inpN(j,:) .*  wInpINH(j,:) ;
-                 % (3) 1-all INH -> Output : SynIn = SynIn + InpN .* wINHout
-                SynIn = SynIn + dot(outN(t-1,j), wINHout(j)) ;   
-                    
+                SynIn = SynIn + dot(inpN(j,:),  wInpINH(j,:)) ;
+                   
             end
+             % (3) 1-all INH -> Output : SynIn = SynIn + InpN .* wINHout
+                SynIn = SynIn + outN(i,t-1).* wINHout(t) ;   
 
-%             outN(t,i) = sigmoidN(SynIn);
-            outN(t,:) = SynIn;
-             
-            outfr(i) = sum(outN,i); 
+            outN(i,t) = sigmoidN(SynIn); 
         end % outN 
-        
     end % end of time loop
+
+
+% figure;
+% plot(Npd,inpN(a,:))
+% hold on
+% contrast(a,:) = inpN(a,:) .* (0.5 / (sigma + 0.5) );
+% plot(Npd,contrast(a,:)); 
+% hold off 
+
+       
 %     outfr(a) = sum(outN(:,a));
 %     figure
 %     plot(Npd,outN(:,i),'-*')
-end % end neuron ANGLE loop 
-    
+% end % end neuron ANGLE loop 
+    outfr=[];    
+    for ii=1:numOut 
+         outfr(ii) = sum(outN(ii,:)); 
+    end
     figure
-    plot(Npd,outfr,'-*')
+    plot(Npd,outfr); 
+%     plot(Npd,outfr,'-*')
     
 
 end % end of user-defined function
@@ -110,8 +116,8 @@ end
 %   Input:  I - synaptic input
 %           g - gain
 %   Output: firing rate of sigmoid function
-% function r = sigmoidN (I) 
-% g=1; 
-% r = 36.03/(1+exp((-I)/.029  ));
-% end
+function r = sigmoidN (I) 
+g=1; 
+r = 36.03/(1+exp((-I)/.029  ));
+end
 
--- midterm1.m
+++ midterm1.m
@@ -8,6 +8,7 @@
 %
 %
 
+
 clear all
 close all
 clc
@@ -127,3 +128,123 @@ mean_fr = sum(outN,2);
     plot(s,outN(7,:,5))
     
     
+%%
+% clear all
+% close all
+% clc
+% 
+% % Gaussian Tuning Curves: 
+% rmax= 52.14; % parameter from Dayan&Abbot 
+% sigma= 14.73; % parameter from Dayan&Abbot 
+% % Make Stimulus:
+% s = [-90:270]; % preferred orientation (degrees) 
+% c = [0;.12;.25;.50;.75]; 
+% 
+% % 7 Input Neurons: InpN 
+% % how do I create input neurons using cosine turning curves? 
+% PrefDir_Neuron = [0,22.5,45,90,112.5,135,180]; % input NEURON orientation angles in degrees
+% 
+% numInpN = length(PrefDir_Neuron);
+% numOutN = length(PrefDir_Neuron);
+% 
+% % allocate and initialize neurons
+% inpN = zeros(numInpN,length(s),length(c)); %  
+% outN = zeros(numOutN,length(s),length(c));
+% inpINH = zeros(numInpN,length(s),length(c)); 
+% 
+% 
+% % allocate and initialize 3 sets of weights: 
+% % 1. Excitatory weights from Input to Output neurons: 
+%     % the input to output weights are set to make horizaontal directions have
+%     % stronger weights projecting to the horizaontal output neuron and vice versa
+%     Vertical = 4; 
+%     w=ones(numInpN,1)*.1;% initialized to .1
+%     w(Vertical) = 3; 
+%     wexc = repmat( w(:), [1,length(s),length(c)] );
+%     
+% 
+% % 2. InputNeurons to INHIBITORY neuron:
+% winp = ones(numInpN,1)*.5;
+% wInpINH = repmat( winp(:),[1,length(s),length(c)] );
+% 
+% % 3. INHIBITORY neuron to OutputNeurons: 
+% winh=ones(numInpN,1)*-1; % initialized to -1 
+% wINHout = repmat( winh(:),[1,length(s),length(c)] ) ; 
+% 
+% % outN(1,:) = rand(1,length(s));    
+% 
+% for a=1:numInpN % neuron's PrefDir loop 
+%        for si=1:length(s) % loop through 361 degrees for inpN activity 
+%            for ci=1:length(c) % loop through the different contrast levels 
+% 
+%                 inpN(a,si,ci) = rmax * c(ci) * exp( (-1/2)* ( ( s(si)-PrefDir_Neuron(a) ) ./ sigma ).^2 );
+%                 inpINH(a,si,ci) = wexc(a,si,ci) .* inpN(a,si,ci); 
+%                 outN(a,si,ci) = inpINH(a,si,ci) .* ( c(ci)/(sigma+c(ci)) );
+%                 
+%            end         
+%         end % 
+%        
+% end % end PrefDir loop  
+%    
+% mean_fr = sum(outN,2); 
+%     figure;
+%     plot(PrefDir_Neuron,mean_fr(:,1,1))
+%     hold on 
+%     plot(PrefDir_Neuron,mean_fr(:,1,2))
+%     plot(PrefDir_Neuron,mean_fr(:,1,3))
+%     plot(PrefDir_Neuron,mean_fr(:,1,4))
+%     plot(PrefDir_Neuron,mean_fr(:,1,5))
+%     
+%     
+%     subplot(2,3,1);
+%     plot(s,outN(1,:,1))
+%     hold on 
+%     plot(s,outN(2,:,1))
+%     plot(s,outN(3,:,1))
+%     plot(s,outN(4,:,1))
+%     plot(s,outN(5,:,1))
+%     plot(s,outN(6,:,1))
+%     plot(s,outN(7,:,1))
+%     
+%     subplot(2,3,2);
+%     plot(s,outN(1,:,2))
+%     hold on 
+%     plot(s,outN(2,:,2))
+%     plot(s,outN(3,:,2))
+%     plot(s,outN(4,:,2))
+%     plot(s,outN(5,:,2))
+%     plot(s,outN(6,:,2))
+%     plot(s,outN(7,:,2))
+%     
+%     subplot(2,3,3);
+%     plot(s,outN(1,:,3))
+%     hold on
+%     plot(s,outN(2,:,3))
+%     plot(s,outN(3,:,3))
+%     plot(s,outN(4,:,3))
+%     plot(s,outN(5,:,3))
+%     plot(s,outN(6,:,3))
+%     plot(s,outN(7,:,3))
+%     
+%     subplot(2,3,4);
+%     plot(s,outN(1,:,4))
+%     hold on 
+%     plot(s,outN(2,:,4))
+%     plot(s,outN(3,:,4))
+%     plot(s,outN(4,:,4))
+%     plot(s,outN(5,:,4))
+%     plot(s,outN(6,:,4))
+%     plot(s,outN(7,:,4))
+% 
+% 
+%     subplot(2,3,5);
+%     plot(s,outN(1,:,5))
+%     hold on 
+%     plot(s,outN(2,:,5))
+%     plot(s,outN(3,:,5))
+%     plot(s,outN(4,:,5))
+%     plot(s,outN(5,:,5))
+%     plot(s,outN(6,:,5))
+%     plot(s,outN(7,:,5))
+%     
+%     
