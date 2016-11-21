093cbd77008bec3c8258e3c1b9d29a01687a8e7e
Colleen <Colleenpamchen@gmail.com>
2016-11-21 10:37:33 -0800

    added final draft

---
+++ FINAL_Colleen_Chen.m
@@ -0,0 +1,28 @@
+%% FINAL EXAM: Colleen Chen 
+%  
+% Neuron network setup:
+%   100 poisson spiking neurons
+%   Excitatory Izhekevich regular spiking (RS)
+%   Inhibitory fast spiking (FS)
+%   delay: 1ms 
+%   initial weights: randn()/uniform distribution [.01 .03]
+%   STDP paramters: 
+%       apre, apost, taupre, taupost 
+% 
+% 
+% synaptic weights
+% firing rates
+% target homeostatsis firing rate: 35hz 
+% 
+% Homeostasis & STDP Learning rule:   
+% 
+% weight update
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
