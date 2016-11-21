8a469057fa2930ca5c06553f77e29cbcf5f0ec83
Colleen <Colleenpamchen@gmail.com>
2016-11-21 10:44:14 -0800

    added jk solution files

---
+++ jk_midterm_da_instrumental.m
@@ -0,0 +1,183 @@
+% Psych268A - Fall 2016
+% Midterm Exam
+%   Dopamine modulated STDP. Replicate Figure 3B from Izhikevich model.
+%   Changes from original are marked with JLK
+
+% daspnet.m: Spiking network with DA-modulated STDP. Based on spnet.m
+% Created by Eugene M.Izhikevich.                November 3, 2004
+%
+% This program reproduces the experiment in Fig.1 in
+% Izhikevich E.M. (2007) Solving the Distal Reward Problem through Linkage
+% of STDP and Dopamine Signaling. Cerebral Cortex, 10.1093/cercor/bhl152
+%
+% n1 - the presynaptic neuron. syn is the synapse to be reinforced.
+% Plot: top - spike raster. Bottom left - synaptic strength (blue), the
+% eligibility trace (green), and the rewards (red x). Bottom right - the
+% distribution of synaptic weights with the chosen synapse marked by red dot.
+
+M=100;                 % number of synapses per neuron
+D=1;                   % maximal conduction delay
+% excitatory neurons   % inhibitory neurons      % total number
+% Ne=800;                Ni=200;                   N=Ne+Ni;
+Ne=400;                Ni=100;                   N=Ne+Ni;
+a=[0.02*ones(Ne,1);    0.1*ones(Ni,1)];
+d=[   8*ones(Ne,1);    2*ones(Ni,1)];
+sm=4;                 % maximal synaptic strength
+
+post=ceil([N*rand(Ne,M);Ne*rand(Ni,M)]);
+s=[ones(Ne,M);-ones(Ni,M)];         % synaptic weights
+sd=zeros(N,M);                      % their derivatives
+for i=1:N
+    if i<=Ne
+        for j=1:D
+            delays{i,j}=M/D*(j-1)+(1:M/D);
+        end;
+    else
+        delays{i,1}=1:M;
+    end;
+    pre{i}=find(post==i&s>0);             % pre excitatory neurons
+    aux{i}=N*(D-1-ceil(ceil(pre{i}/N)/(M/D)))+1+mod(pre{i}-1,N);
+end;
+STDP = zeros(N,1001+D);
+v = -65*ones(N,1);                      % initial values
+u = 0.2.*v;                             % initial values
+firings=[-D 0];                         % spike timings
+
+%---------------
+% new stuff related to DA-STDP
+T=800*10;       % 800 trials, 10 seconds per trial
+DA=0;           % level of dopamine above the baseline
+rew=[];
+
+
+% JLK set up groups and parameters for midterm experiment.
+groupS = 1:50;
+groupA = 51:100;
+groupB = 101:150;
+startTrial = 0;
+endTrial = 0;
+responseA = 0;
+responseB = 0;
+trials = 0;
+activityGroupA = 0;
+activityGroupB = 0;
+% JLK
+
+
+% JLK - change from the original. 
+interval = 20;  % 20 millisecond interval to give the stimulus response.
+%--------------
+
+for sec=1:T                             % simulation of 1 day
+    for t=1:1000                          % simulation of 1 sec
+        I=13*(rand(N,1)-0.5);               % random thalamic input
+        fired = find(v>=30);                % indices of fired neurons
+        
+        % ****************************************************************
+        % JLK - new code for injecting a stimulus.
+        
+        % Trial starts every 10 seconds
+        if mod(sec,10) == 0 && t == 1
+            startTrial = sec*1000+t;
+            endTrial = startTrial + interval;   % the trial end 20 ms from now
+            I(groupS) = I(groupS) + 15;         % increase the synaptic input to cause stimulus group to fire
+            activityGroupA = 0;
+            activityGroupB = 0;
+            trials = trials + 1;    % keep track of the trial number to get a probabiltiy of response
+        end
+        
+        % Over the 20 millisecond trial count the spikes in group A and
+        % group B.
+        if sec*1000+t > startTrial && sec*1000+t < endTrial
+            activityGroupA = activityGroupA + sum(v(groupA) >= 30);
+            activityGroupB = activityGroupB + sum(v(groupB) >= 30);
+            
+        % End of trial. If its the first half of the experiment and the A
+        % group has more spikes than the B group, set a reward sometime in
+        % the future. In the second half, reward if B > A.
+        elseif sec*1000+t == endTrial
+            if sec < T/2 && activityGroupA >= 1*activityGroupB
+                rew = round(sec*1000+t + (1-activityGroupA/(activityGroupA+activityGroupB))*1000);
+                disp('REWARD A')
+            elseif sec > T/2 && activityGroupB >= 1*activityGroupA
+                rew = round(sec*1000+t + (1-activityGroupB/(activityGroupA+activityGroupB))*1000);
+                disp('REWARD B')
+            end
+            
+            % count the responses for each group.
+            if activityGroupA > activityGroupB
+                responseA = responseA + 1;
+            else
+                responseB = responseB + 1;
+            end
+            
+            % calculate a running probability of A and B responses.
+            pA(trials) = responseA/trials;
+            pB(trials) = responseB/trials;
+            
+            % plot the probability of A and B responses
+            subplot(2,1,2)
+            plot(1:trials,pA,1:trials,pB)
+            legend(['prob(response A)';'prob(response B)'])
+            disp(['t=', num2str(sec*1000+t), ' |A|=', num2str(activityGroupA),' |B|=', num2str(activityGroupB), ' pA=', num2str(pA(trials)), ' pB=', num2str(pB(trials))])
+        end
+        % ****************************************************************
+
+        v(fired)=-65;
+        u(fired)=u(fired)+d(fired);
+        STDP(fired,t+D)=0.1;
+        for k=1:length(fired)
+            sd(pre{fired(k)})=sd(pre{fired(k)})+STDP(N*t+aux{fired(k)});
+        end;
+        firings=[firings;t*ones(length(fired),1),fired];
+        k=size(firings,1);
+        while firings(k,1)>t-D
+            del=delays{firings(k,2),t-firings(k,1)+1};
+            ind = post(firings(k,2),del);
+            I(ind)=I(ind)+s(firings(k,2), del)';
+            sd(firings(k,2),del)=sd(firings(k,2),del)-1.5*STDP(ind,t+D)';
+            k=k-1;
+        end;
+        v=v+0.5*((0.04*v+5).*v+140-u+I);    % for numerical
+        v=v+0.5*((0.04*v+5).*v+140-u+I);    % stability time
+        u=u+a.*(0.2*v-u);                   % step is 0.5 ms
+        STDP(:,t+D+1)=0.95*STDP(:,t+D);     % tau = 20 ms
+        
+        DA=DA*0.995;
+        if (mod(t,10)==0)
+            s(1:Ne,:)=max(0,min(sm,s(1:Ne,:)+(0.002+DA)*sd(1:Ne,:)));
+            sd=0.99*sd;
+        end;
+        
+        % JLK commented out this section from the original experiment
+        %     if any(fired==n1)
+        %         n1f=[n1f,sec*1000+t];
+        %     end
+        %     if any(fired==n2)
+        %         n2f=[n2f,sec*1000+t];
+        %         if (sec*1000+t-n1f(end)<interval) & (n2f(end)>n1f(end))
+        %             rew=[rew,sec*1000+t+1000+ceil(2000*rand)];
+        %         end;
+        %     end
+        if rew==sec*1000+t
+            DA=DA+0.5;
+            disp(['Reward applied at time ', num2str(rew)])
+        end;
+        
+    end;
+    % ---- plot -------
+    subplot(2,1,1)
+    plot(firings(:,1),firings(:,2),'.');
+    axis([0 1000 0 N]);
+    
+    %   subplot(2,2,3);
+    %   plot(0.001*(1:(sec*1000+t)),shist(1:sec*1000+t,:), 0.001*rew,0*rew,'rx');
+    %   subplot(2,2,4);
+    %   hist(s(find(s>0)),sm*(0.01:0.01:1)); % only excitatory synapses
+    %   hold on; plot(s(n1,syn),0,'r.'); hold off;
+    drawnow;
+    % ---- end plot ------
+    STDP(:,1:D+1)=STDP(:,1001:1001+D);
+    ind = find(firings(:,1) > 1001-D);
+    firings=[-D 0;firings(ind,1)-1000,firings(ind,2)];
+end;
---
+++ jk_midterm_divisive_normalization.m
@@ -0,0 +1,186 @@
+% Psych268A - Fall 2016
+% Midterm Exam
+%   Demontrate divisive normalization in a small network
+function jk_midterm_divisive_normalization
+
+% Preferred Directions
+global pd;
+pd = [ -pi/2 -pi/4 -pi/8 0 pi/8 pi/4 pi/2 ];
+
+% Baseline Firing Rate
+global r0;
+r0 = 36;
+
+% Calculate the cosine tuning curves for visualization purposes
+for i = 1:size(pd,2)
+    inx = 0;
+    for j = -pi:0.1:pi
+        inx = inx + 1;
+        r(inx) = cosineTuning (j, r0, 60, pd(i));
+    end
+    plot(r)
+    hold on
+end
+
+% Give a range of maximum firing rates to simulate contrast. Low rMax
+% represents low contrast and high rMax represents high contrast
+rMax = [40, 60, 80, 100 ];
+
+figure;
+
+% Replicate Figure 3e from Carandini and Heeger
+%   Top row has one stimulus
+stimulus = 0;
+for i = 1:size(rMax,2)
+    fr = runDivisiveNormalization(rMax(i), stimulus, 1);
+    subplot(2,size(rMax,2),i)
+    plot(fr)
+    axis([0 size(fr,2)+1 0 1])
+    title(['Div Norm - Rmax = ', num2str(rMax(i))])
+end
+
+%   Bottom row has two stimuli
+stimulus = [pi/2; -pi/2];
+for i = 1:size(rMax,2)
+    fr = runDivisiveNormalization(rMax(i), stimulus, 1);
+    subplot(2,size(rMax,2),i+size(rMax,2))
+    plot(fr)
+    axis([0 size(fr,2)+1 0 1])
+    title(['Div Norm - Rmax = ', num2str(rMax(i))])
+end
+
+figure;
+
+% Re-run the model without normalization
+stimulus = 0;
+for i = 1:size(rMax,2)
+    fr = runDivisiveNormalization(rMax(i), stimulus, 0);
+    subplot(2,size(rMax,2),i)
+    plot(fr)
+    axis([0 size(fr,2)+1 0 1])
+    title(['No Norm - Rmax = ', num2str(rMax(i))])
+end
+
+stimulus = [pi/2; -pi/2];
+for i = 1:size(rMax,2)
+    fr = runDivisiveNormalization(rMax(i), stimulus, 0);
+    subplot(2,size(rMax,2),i+size(rMax,2))
+    plot(fr)
+    axis([0 size(fr,2)+1 0 1])
+    title(['No Norm - Rmax = ', num2str(rMax(i))])
+end
+
+end
+
+function fr = runDivisiveNormalization(rMax, stimulus, divisiveNormalization)
+
+global r0
+global pd;
+numInput = size(pd,2);
+numOutput = numInput;
+numInhibitory = 1;
+nInput = zeros(1,numInput);
+nOutput = zeros(1,numOutput);
+nInhibitory = zeros(1,numInhibitory);
+rHighContrast = 100;
+
+gain = 0.1;
+
+% If there is not divisive normalization set the weights lower to get good
+% activity levels. Using the same weights maxed out activity for all
+% neurons.
+if divisiveNormalization
+    W_INP_OUT = 1;
+else
+    W_INP_OUT = 0.10;
+end
+wInpOut = zeros(numInput,numOutput);
+for i = 1:numInput
+    wInpOut(i,i) = W_INP_OUT;
+end
+
+% There is a many to one projection from the input neurons to the inhibitory
+% neuron. So, the weight is relatively weak.
+W_INP_INH = 0.002;
+wInpInh = zeros(numInput,numInhibitory);
+for i = 1:numInput
+    for j = 1:numInhibitory
+        wInpInh(i,j) = W_INP_INH;
+    end
+end
+
+% There is a one to many projection from the inhibitory neuron to the output neurons.
+% So the weight is negative and strong.
+W_INH_OUT = -125;
+wInhOut = zeros(numInhibitory,numOutput);
+for i = 1:numInput
+    for j = 1:numInhibitory
+        wInhOut(i,j) = W_INH_OUT;
+    end
+end
+
+
+T = 10;
+for ts = 1:T
+    
+    % Make a copy of the previous activity. Use this when calculating
+    % activity
+    nInhibitoryPrev = nInhibitory;
+    nInputPrev = nInput;
+    
+    % Get the firing rate, based on a cosine tuning curve, for each input neuron.
+    for i = 1:numInput
+        
+        % If there are 2 stimuli, the second stimulus is a high contrast
+        % input.
+        if size(stimulus,1) == 2
+            nInput(i) = cosineTuning(stimulus(1), r0, rMax, pd(i)) + cosineTuning(stimulus(2), r0, rHighContrast, pd(i));
+        else
+            nInput(i) = cosineTuning(stimulus(1), r0, rMax, pd(i));
+        end
+    end
+    
+    % Get the inhibitory neuron activity. The synaptic input is from the 
+    % input neuron activity times the weights. Use a sigmoid function to get the
+    % activity. If there is no divisive normalization, set the inhibitory
+    % activity to zero.
+    for i = 1:numInhibitory
+        if (divisiveNormalization)
+            nInhibitory(i) = sigmoid(sum(nInputPrev .* wInpInh(i,:)), gain);
+        else
+            nInhibitory(i) = 0;
+        end
+    end
+    
+    % Get the output neuron activity. The synaptic input comes from the
+    % input layer and the inhibitory neuron. Run this through a sigmoid
+    % function.
+    for i = 1:numOutput
+        nOutput(i) = sigmoid(sum(nInput .* wInpOut(i,:))+sum(nInhibitoryPrev .* wInhOut(i,:)), gain);
+    end
+end
+
+fr = nOutput;
+
+disp(['Rmax = ', num2str(rMax), ' Divisive Normalization = ', num2str(divisiveNormalization)])
+fprintf(1,'   Input: ');
+fprintf(1,'%3.3f ', nInput); fprintf('\n')
+fprintf(1,'   Inhibitory: ');
+fprintf(1,'%3.3f ', nInhibitory); fprintf('\n')
+fprintf(1,'   Output: ');
+fprintf(1,'%3.3f ', nOutput); fprintf('\n')
+fprintf('\n')
+
+end
+
+function s = sigmoid(synIn, gain)
+
+s = 1/(1 + exp(-gain*synIn));
+
+end
+
+function f = cosineTuning (input, r0, rMax, sMax)
+
+f = max(0, r0 + (rMax - r0) * cos(input - sMax));
+
+end
