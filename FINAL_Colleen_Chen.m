7e82f3c31a67d2fca2141036cd40e42bba4daf53
Colleen <Colleenpamchen@gmail.com>
2016-12-02 03:26:42 -0800

    it's working

--- FINAL_Colleen_Chen.m
+++ FINAL_Colleen_Chen.m
@@ -4,16 +4,14 @@
 %       non random stimulus.  
 % ( ) alpha- homeostatic scaling factor??  
 % ( ) R average firing rate of the postsynpatic neuron j
-% ( ) gamma- tuning factor =50
-%
 
 clear all
 close all
 clc
 
 % ADD NEW homeostatic variables: 
-alpha = 100; % homeostatic scaling factor? (not given) 
-R = 0; % average firing rate of postsynaptic neuron j = OUTPUT neuron 
+alpha = 1; % homeostatic scaling factor? (not given) 
+R = 36; % average firing rate of postsynaptic neuron j = OUTPUT neuron 
 T = 5; %5000 ; % ms. %T=5 SECONDS. time scale over which the firing rate of the postsunaptic  neuron was averaged 
 Rtarget= 35; % 35 Hz target firing rate 
 gamma = 50; % homeostasis tuning factor 
@@ -37,10 +35,10 @@ A_minus = 0.000066;
 t_plus = 20;
 t_minus = 60;
 
-Time=100;  % conveninetly, both simulation seconds and timestep ms 
+Time  =      1000;  % conveninetly, both simulation seconds and timestep ms 
 
 wmin = 0;
-wmax = 0.03;
+wmax = 0.041;
 
 v1 = -65 ; % there is only 1 v
 u1 = b*v1;  % Initial values of u
@@ -56,7 +54,7 @@ for i = 1:N
     end
 end
 
-fr1 = zeros(Time+1,1); % FIRING RATE keeps track of how many cycles per second output neuron fired 
+fr1 = zeros(Time,1); % FIRING RATE keeps track of how many cycles per second output neuron fired 
 I1=zeros(N,1); 
 
 % [ ] keep track of the time steps when the poisson spiked. 
@@ -65,74 +63,84 @@ I1=zeros(N,1);
 % H= firing rate of 64 neurons  [64, 1] 
 % [784,64] * [64,1] = [784, 1] image 
 
-% INITIALIZE poisson, simulate 5 sec of poison spikes:
-spikes1 = zeros(N,2); % input POISSON spikes into the system 
+
 count1 = zeros(N,1);
 lambda=[0.2:0.2:20]'; % mean Firing Rate of poisson neurons per SEC   
-xrand=rand(N,1); 
+
+spikes1=zeros(N,1);
+xrand = rand(N,1); 
 spikes1(:,2) =  spikes1(:,1) - ( log(xrand)./lambda ) ; % this is generating 'tau' the interspike intervals
 
-for sec = 2:Time+1  % 1000 simulation seconds 
+for sec = 1:Time  % 1000 simulation seconds 
 %     disp(sec);
     dsdt = zeros(N,1);
-    R = fr1(sec-1);
-    K = R / ( T.*( 1+ abs(1-R/Rtarget)*gamma ) ) ;
+    if sec<2
+        R=36;
+    else
+    R = fr1(sec);
+    end
     
+    K = R / ( T.*( 1+ abs(1-R/Rtarget)*gamma ) ) ;
+
     for t=1:1000        % simulation of 1000 ms   
-        % IF POISSON SPIKED        
-        Ifired1 = spikes1(:,2) <= t/1000+sec ; % logical array that allows you to index 
-if ~isempty(Ifired1) 
-        spikes1(Ifired1,1) = spikes1(Ifired1,2);
-        xrand = rand(sum(Ifired1),1); 
-        spikes1(Ifired1,2) = spikes1(Ifired1,1) - (log(xrand)./lambda(Ifired1)) ;     
-        %index of all poisson i's that spiked:
-        pidx = find(Ifired1); 
+        % IF POISSON SPIKED  
+         Ifired1 = spikes1(:,2) <= t/1000+sec ; % logical array that allows you to index 
         
-            count1(Ifired1) = count1(Ifired1)+1;
-            numFired = sum(Ifired1);
+    if ~isempty(Ifired1) 
+            spikes1(Ifired1,1) = spikes1(Ifired1,2);
+            xrand = rand(sum(Ifired1),1); 
+            spikes1(Ifired1,2) = spikes1(Ifired1,1) - (log(xrand)./lambda(Ifired1)) ;     
+    
+            pidx1 = find(Ifired1); %         index of all poisson i's that spiked:
+
+            count1(Ifired1) = count1(Ifired1)+1;   % total number of times each poisson neuron fired 
+            numFired1 = sum(Ifired1);              % number of poisson neurons that fired at this time step
             % update voltage
             I1(Ifired1) = I1(Ifired1)+ S1(Ifired1); 
-            v1 = v1 + 0.5*(0.04* v1^2 +5*v1 +140 -u1 + sum(I1(Ifired1)) ) ;
-            v1 = v1 + 0.5*(0.04* v1^2 +5*v1 +140 -u1 + sum(I1(Ifired1)) ) ;
-            u1 = u1 + a.*(b.*v1-u1);
-%                 disp(v1)       
-            if v1 >=30 
-               disp('vfired!')
-               fr1(sec)=fr1(sec)+1;
-               v1 = c; % reset V 
-               u1 = u1 + d;
-               dsdt = dsdt+ (alpha .* S1 .* (1 - R/Rtarget) + 1.*(LTP1)).*K;
-               LTP(Ifired1) = A_plus;
-            end
-            
-            dsdt = dsdt+ (alpha .* S1 .* (1 - R/Rtarget) + 1.*(LTD1)).*K;
-            LTD(Ifired1) = A_minus;
-            
-            LTP1 = LTP1 - LTP1/t_plus;
-            LTD1 = LTD1 - LTD1/t_minus;
-end % end of Ifired1 
+
+                if v1 >=30 
+                   fr1(sec)=fr1(sec)+1;
+                   v1 = c; % reset V 
+                   u1 = u1 + d;
+
+                   % LTP to those that fired, LTD to all else 
+%                    dsdt = dsdt+ ( alpha .* S1 .* (1 - R/Rtarget) + 1.*(LTP1 + LTD1) ).* K ;
+                     dsdt = dsdt+ K.* ( alpha .* S1 .* (1 - R/Rtarget) ) + K.*(LTP1 + LTD1)  ;
+%                    dsdt = dsdt+ (alpha .* S1 .* (1 - R/Rtarget) + 1.*(LTD1)).* K;
+                   LTP1(Ifired1) = A_plus;
+%                    LTD1(Ifired1) = A_minus;
+
+                end % if V fired...     
+                dsdt = dsdt+ K.*(alpha .* S1 .* (1 - R/Rtarget) ) + K.*(LTD1) ;
+                LTD1(Ifired1) = A_minus;
+        end % end of Ifired1 
+        v1 = v1 + 0.5*( 0.04* v1^2 +5*v1 +140 -u1 + sum(I1(Ifired1)) ) ;
+        v1 = v1 + 0.5*(0.04* v1^2 +5*v1 +140 -u1 + sum(I1(Ifired1)) ) ;
+        u1 = u1 + a.*(b.*v1-u1);
+
+        LTP1 = LTP1 - LTP1/t_plus;
+        LTD1 = LTD1 - LTD1/t_minus;
     end % end of t ms loop 
 
         S1 = S1 + dsdt;  
-        S1 = max(wmin, S1 );    
-   
+%         S1 = max(wmin, S1 ) ;
+        S1 = max(wmax, S1 ) ;
+  
    
 end % End of TIME second loop 
-poisson_firing_rate_given= count1/Time;
+poisson_firing_rate_given1 = count1/Time;
 
-subplot(2,2,1)
-plot((1:Time), fr1(2:Time+1)) % time, firing rate
-title('HOMEOSTASIS')
-% figure
-subplot(2,2,2)
-plot((1:100),S1,'+r')    % synapse ID number, synaptic strength 
-title('HOMEOSTASIS')
-ylim([-0.01 0.06]) 
+% subplot(2,2,1)
+% plot((1:Time), fr1/28) % time, firing rate
+% title('HOMEOSTASIS')
+% % figure
+% subplot(2,2,2)
+% plot((1:100),S1,'+r')    % synapse ID number, synaptic strength 
+% title('HOMEOSTASIS')
+% ylim([-0.001 0.04]) 
 
 
-%% Standard Izzy STDP 
-% FIRST NETWORK CONFIG WITHOUT HOMEOSTASIS: 
-% IMPLEMENT A all-to-one connections: 
+% .......................................................
 
 S = zeros(N,1); % [poisson neurons 100 x 1 izzy output neuron] 
 aa=.01; bb=.03;
@@ -149,63 +157,168 @@ v = -65;
 u = b*v;  % Initial values of u
 
 fr = zeros(Time,1); % FIRING RATE keeps track of how many cycles per second output neuron fired 
-spikes = zeros(N,Time); % input POISSON spikes into the system 
-fired=[];
-I=zeros(N,1); 
-% nspikes = zeros(N,1);
+% spikes = zeros(N,Time); % input POISSON spikes into the system 
+Ifired=[];
+I=zeros(N,1);  
 
-lambda=[0.2:0.2:20]; % mean Firing Rate of poisson neurons 
-for i=1:N
-   for t=1:1000 % I guess this is looping through the Time trials from 1:1000 SECONDS
-      xrand = rand(1);
-      % this is generating 'tau' the interspike intervals 
-      spikes(i,t+1) =  spikes(i,t) - ( log(xrand)/lambda(i) ) ; % generate 1000 spikes per neuron in SECONDs  
-   end
-end
-spikes = ceil(spikes); % these are the spike times in ms.  
+% INITIALIZE poisson, simulate 5 sec of poison spikes:
+spikes = zeros(N,2); % input POISSON spikes into the system 
+count = zeros(N,1);
+
+lambda=[0.2:0.2:20]'; % mean Firing Rate of poisson neurons per SEC   
+xrand=rand(N,1); 
+spikes(:,2) =  spikes(:,1) - ( log(xrand)./lambda ) ; % this is generating 'tau' the interspike intervals
 
 for sec = 1:Time  % 1000 simulation seconds 
     disp(sec);
-    vfired=[]; 
-    
-    for t=1:1000 %1000          % simulation of 1000 ms
+    firings = []; 
+    for t=1:1000        % simulation of 1000 ms   
+        % IF POISSON SPIKED    
+%         t/1000+sec 
+        Ifired = spikes(:,2) <= t/1000+sec ; % logical array that allows you to index 
+        
+        if ~isempty(Ifired) 
+        firings = [firings; t+0*Ifired,Ifired];
+        spikes(Ifired,1) = spikes(Ifired,2);
+        xrand = rand(sum(Ifired),1); 
+        spikes(Ifired,2) = spikes(Ifired,1) - (log(xrand)./lambda(Ifired)) ;     
+        pidx = find(Ifired); % index of all poisson i's that spiked      
+        count(Ifired) = count(Ifired)+1; 
+        numFired = sum(Ifired);
+%             str = [num2str( numFired), ' poisson fired at ', num2str(t) ];
+%             disp(str);           
+           
+            % update voltage
+            I(Ifired) = I(Ifired)+ S(Ifired); 
        
-        for i = 1:N  % this loops through the poisson input NEURONS 
-          
-            if v >=30
-                fr(sec)=fr(sec)+1;
-                v = c;
-                u = u + d;   
-                vfired = [vfired; t]; % v neuron fired at t 
-%                  disp('vfired!')
-                S(i) = max(wmin, S(i) + LTD(i)); % LTD to weights that connect to all exc from i
-                LTD(i) = A_minus;  % set max LTD to i from all exc
-            end 
-            
-                Ifired1 = find(spikes(i,1:Time+1)==t); % the t indices of ith poisson that fired 
-                if ~isempty(Ifired1) 
-                        I(i) = I(i) + S(i);  %*length(fired) ; % If at this timestep, neuron i spiked, then add input and synaptic weight
-                       
-                        S(i) = min(wmax, S(i)+ LTP(i)); % LTP to weights that connect to i from all exc 
-                        LTP(i) = A_plus;   % set max LTP to all exc from i          
-                end
-             v = v + 0.5* (0.04 * v^2 + 5*v + 140 -u + I(i) );
-             v = v + 0.5*(0.04 * v^2 + 5*v + 140 -u + I(i) );       
-             u = u + a*(b*v -u ); 
-        end % poissonNeuron i loop
-        LTP = LTP - LTP/t_plus;
-        LTD = LTD - LTD/t_minus;
-    end % end of t ms loop    
-    
-end % end of Time s loop 
+            if v >=30 
+               fr(sec)=fr(sec)+1;
+               v = c; % reset V 
+               u = u + d;
+               S(Ifired) = min(wmax, S(Ifired) + LTP(Ifired)); % LTP to weights that connect to all exc from i
+                % S = min(wmax, S + LTP); % LTP to weights that connect to all exc from i
+               LTP(Ifired) = A_plus;  % set max LTD to i from all exc
+                %             S(~Ifired) = max(wmin, S(~Ifired)+ LTD(~Ifired)); % LTP to weights that connect to i from all exc  
+                % % S(Ifired) = max(wmin, S(Ifired)+ LTD(Ifired));
+                %             LTD(Ifired) = A_minus;   
+            end
+            S(Ifired) = max(wmin, S(Ifired)+ LTD(Ifired)); % LTP to weights that connect to i from all exc  
+            % S(Ifired) = max(wmin, S(Ifired)+ LTD(Ifired));
+            LTD(Ifired) = A_minus;
 
-% figure
-subplot(2,2,3)
-plot([1:Time], fr./5 ) % time, firing rate
-% figure
-subplot(2,2,4)
-plot([1:100],S,'+r')    % synapse ID number, synaptic strength 
-ylim([-0.01 0.06])    
+end % end of Ifired1 
+            v = v + 0.5*(0.04* v^2 +5*v +140 -u + sum(I(Ifired)) ) ;
+            v = v + 0.5*(0.04* v^2 +5*v +140 -u + sum(I(Ifired)) ) ;
+            u = u + a.*(b.*v-u);
+            
+            LTP = LTP - LTP/t_plus;
+            LTD = LTD - LTD/t_minus;
+end % end of t ms loop 
+  
+        S = max(wmin, S);    
+   
+end % End of TIME second loop 
+poisson_firing_rate_given = count/Time;
+
+% subplot(2,2,3)
+% plot([1:Time], fr ) % time, firing rate
+% subplot(2,2,4)
+% plot([1:100],S,'+r')    % synapse ID number, synaptic strength 
+% % ylim([-0.01 0.06]) 
+
+subplot(1,2,2)
+plot((1:Time), fr1 ,'r') % time, firing rate
+title('Firing rate of output neuron with and without Homeostasis enabled')
+hold on
+plot([1:Time], fr ,'b') % time, firing rate
+hold off
+%...
+subplot(1,2,1)
+plot([1:100],S,'+b')    % synapse ID number, synaptic strength 
+title('Synaptic strength of synapses with and without Homeostasis enabled')
+hold on
+% ylim([-0.01 0.06])  
+plot((1:100),S1,'+r')    % synapse ID number, synaptic strength 
+hold off
+% ylim([-0.001 0.04]) 
+
+
+%% Standard Izzy STDP 
+% FIRST NETWORK CONFIG WITHOUT HOMEOSTASIS: 
+% IMPLEMENT A all-to-one connections: 
+
+% S = zeros(N,1); % [poisson neurons 100 x 1 izzy output neuron] 
+% aa=.01; bb=.03;
+% for i = 1:N
+%         S(i) = (aa + (bb-aa).*rand(1,1) ) ; % Synaptic weights 100 x 1 outputNeuron 
+% end
+% 
+% % STDP lists for LTD and LTP weight changes
+% LTP(1:Ne, 1:Nout) = 0;    % ordered from, to
+% LTD(1:Ne, 1:Nout) = 0;    % ordered from, to
+% 
+% % there is only 1 v 
+% v = -65; 
+% u = b*v;  % Initial values of u
+% 
+% fr = zeros(Time,1); % FIRING RATE keeps track of how many cycles per second output neuron fired 
+% spikes = zeros(N,Time); % input POISSON spikes into the system 
+% fired=[];
+% I=zeros(N,1); 
+% % nspikes = zeros(N,1);
+% 
+% lambda=[0.2:0.2:20]; % mean Firing Rate of poisson neurons 
+% for i=1:N
+%    for t=1:1000 % I guess this is looping through the Time trials from 1:1000 SECONDS
+%       xrand = rand(1);
+%       % this is generating 'tau' the interspike intervals 
+%       spikes(i,t+1) =  spikes(i,t) - ( log(xrand)/lambda(i) ) ; % generate 1000 spikes per neuron in SECONDs  
+%    end
+% end
+% spikes = ceil(spikes); % these are the spike times in ms.  
+% 
+% for sec = 1:Time  % 1000 simulation seconds 
+%     disp(sec)
+%     vfired=[]; 
+%     
+%     for t=1:1000 %1000          % simulation of 1000 ms
+%        
+%         for i = 1:N  % this loops through the poisson input NEURONS 
+%           
+%             if v >=30
+%                 fr(sec)=fr(sec) + 1;
+%                 v = c;
+%                 u = u + d;   
+%                 vfired = [vfired; t]; % v neuron fired at t 
+% %                  disp('vfired!')
+%                 S(i) = min(wmax, S(i) + LTP(i)); % LTP to weights that connect to all exc from i
+%                 LTP(i) = A_minus;  % set max LTD to i from all exc
+%             end 
+%             
+%                 Ifired = find(spikes(i,1:Time+1)==t); % the t indices of ith poisson that fired 
+%                 if ~isempty(Ifired) 
+%                         I(i) = I(i) + S(i) ;%* S(Ifired);  %*length(fired) ; % If at this timestep, neuron i spiked, then add input and synaptic weight
+%                        
+%                         S(i) = max(wmin, S(i)+ LTD(i)); % LTP to weights that connect to i from all exc 
+%                         LTD(i) = A_plus;   % set max LTP to all exc from i          
+%                 end
+%              v = v + 0.5* (0.04 * v^2 + 5*v + 140 -u + I(i) );
+%              v = v + 0.5*(0.04 * v^2 + 5*v + 140 -u + I(i) );       
+%              u = u + a*(b*v -u ); 
+%         end % poissonNeuron i loop
+%         LTP = LTP - LTP/t_plus;
+%         LTD = LTD - LTD/t_minus;
+%     end % end of t ms loop    
+%     
+% end % end of Time s loop 
+% 
+% % figure
+% subplot(2,2,3)
+% plot([1:Time], fr./5 ) % time, firing rate
+% % figure
+% subplot(2,2,4)
+% plot([1:100],S,'+r')    % synapse ID number, synaptic strength 
+% ylim([-0.01 0.06])    
 
 
 % 
